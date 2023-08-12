6327c57d8a4163d45b7bd57969f8a7be881409e3
Nuwan Rathnayake <x@nuwanr.com>
2023-08-12 23:12:16 +0530

    js operators & data types

---
+++ asssets/js/script.js
@@ -0,0 +1,88 @@
+var name = "nuwan";
+let marks = 10;
+const passingMark = 25;
+
+let a = 1
+let b = 2
+let c = 4
+let d = 11
+/*
+console.log(a+b)
+console.log(b-a)
+console.log(b*c)
+console.log(b/a)
+console.log(b**c)
+console.log(d%c)
+
+console.log(a++)
+console.log(a)
+
+console.log(++b)
+console.log(b)
+
+console.log(a)
+console.log(a--)
+console.log(a)
+
+console.log(b)
+console.log(--b)
+console.log(b)
+*/
+
+let p = 10
+let q = 20
+let r = 30
+
+// p = p+q
+p *= q
+console.log(p)
+
+// String
+let s = "lorem ipsum"
+console.log(typeof s)
+
+// Number
+let n = 10.454
+console.log(typeof n)
+
+// BigInt
+let bi = 1234567890123456789012345678901234567890n
+console.log(typeof bi)
+
+// Boolean
+let bool = true //false //0 //1
+console.log(typeof bool)
+
+// undefined
+let u
+console.log(typeof u)
+
+// null
+let nu = null
+console.log(typeof nu)
+
+// Symbol
+
+// Object
+let obj = {
+    name: "nuwan",
+    age: 25,
+    address: "colombo"
+}
+console.log(typeof obj)
+
+// Array
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(typeof arr)
+
+
+console.log(25+'nuwan');
+console.log("25"+'nuwan');
+
+console.log(25+'10');
+console.log("25"+'10');
+console.log(25-'10');
+
+console.log(5+25+'nuwan')
+console.log('nuwan'+5+25)
+console.log(5+25+'nuwan'+7+8)
